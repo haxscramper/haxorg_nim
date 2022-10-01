a792ee45da46b6a2083b3b3479df934a654ec0b9
haxscramper <haxscramper@gmail.com>
2022-10-02 00:00:06 +0400

    enum for built-in properties

--- src/haxorg/parse_org_common.nim
+++ src/haxorg/parse_org_common.nim
@@ -6,6 +6,44 @@ const
   OCommandChars* = IdentChars + {'-', '_'}
 
 type
+  OrgPropertyKind* = enum
+    ## Built-in and user-provided properites that can be attached to the
+    ## subtrees, files.
+    # properties listed in the 'org-default-properties'
+    opkArchive
+    opkCategory
+    opkSummary
+    opkDescription
+    opkCustomId
+    opkLocation
+    opkLogging
+    opkColumns
+    opkVisibility
+    opkTableExportFormat
+    opkTableExportFile
+    opkExportOptions
+    opkExportText
+    opkExportFileName
+    opkExportTitle
+    opkExportAuthor
+    opkExportDate
+    opkUnnumbered
+    opkOrdered
+    opkNoblocking
+    opkCookieData
+    opkLogIntoDrawer
+    opkRepeatToState
+    opkClockModelineTotal
+    opkStyle
+    opkHtmlContainerClass
+
+    # mentioned in the manual
+    opkEffort
+    opkEffortAll
+    opkCreated
+
+
+  
   OrgCommandKind* = enum
     ## Built-in org commands (single and multiline) such as `#+include`
     ##
@@ -59,6 +97,42 @@ func dashNormalize*(str: string): string =
   for ch in str:
     if ch in {'a' .. 'z', 'A' .. 'Z'}:
       result.add toLowerAscii(ch)
+
+func classifyProperty*(str: string): OrgPropertyKind =
+  let norm = str.strip(chars = {':', '+'}).dashNormalize()
+  case norm:
+    of "archive": opkArchive
+    of "summary": opkSummary
+    of "description": opkDescription
+    of "customid": opkCustomId
+    of "location": opkLocation
+    of "logging": opkLogging
+    of "columns": opkColumns
+    of "visibility": opkVisibility
+    of "tableexportformat": opkTableExportFormat
+    of "tableexportfile": opkTableExportFile
+    of "exportoptions": opkExportOptions
+    of "exporttext": opkExportText
+    of "exportfilename": opkExportFileName
+    of "exporttitle": opkExportTitle
+    of "exportauthor": opkExportAuthor
+    of "exportdate": opkExportDate
+    of "unnumbered": opkUnnumbered
+    of "ordered": opkOrdered
+    of "nonblocking": opkNoblocking
+    of "cookiedata": opkCookieData
+    of "logintodrawer": opkLogIntoDrawer
+    of "repeattostate": opkRepeatToState
+    of "clockmodelinetotal": opkClockModelineTotal
+    of "style": opkStyle
+    of "htmlcontainerclass": opkHtmlContainerClass
+
+    of "effort": opkEffort
+    of "effortall": opkEffortAll
+    of "created": opkCreated
+
+    else:
+      raise newImplementKindError(norm)
 
 func classifyCommand*(str: string): OrgCommandKind =
   let norm = str.dashNormalize()
